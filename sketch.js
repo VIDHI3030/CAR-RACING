65a30075dc1dad49e2699471ab50cae283c98818
VIDHI3030 <72209528+VIDHI3030@users.noreply.github.com>
2020-11-25 18:35:20 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,31 @@
+var canvas, backgroundImage;
 
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
 var database;
-var gameState=0;
-var form,game,player,playerCount;
-var allPlayer;
-function setup(){
-    database=firebase.database();
-    createCanvas(500,500);
-    game=new Game();
-    game.getstate();
-    game.start();
 
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
 }
+
 
 function draw(){
-
-    //background("white");
-    if(playerCount==4){
-        game.update(1);
-    }
-    if(gameState==1){
-        clear ();
-        game.play();
-    }
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
 }
-
