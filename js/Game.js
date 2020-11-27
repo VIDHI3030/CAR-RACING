49e623543a8633cec101bdedc1c4c768d0036ebc
VIDHI3030 <72209528+VIDHI3030@users.noreply.github.com>
2020-11-27 16:17:54 +0530

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -68,7 +68,10 @@ class Game {
         cars[index-1].y = y;
 
         if (index === player.index){
-          cars[index - 1].shapeColor = "red";
+          stroke (10) ;
+          fill ("blue");
+          ellipse(x,y,60,60);
+          
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y
         }
